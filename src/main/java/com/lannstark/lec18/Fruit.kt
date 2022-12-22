466315d3755021f4c7127203e19acec524deaa63
prolkh <prolkh@gmail.com>
2022-12-22 23:52:25 +0900

    코틀린에서 컬렉션을 함수형으로 다루는 방법을 공부하다. (3)

---
+++ src/main/java/com/lannstark/lec18/Fruit.kt
@@ -0,0 +1,13 @@
+package com.lannstark.lec18
+
+data class Fruit (
+    val id: Long,
+    val name: String,
+    val factoryPrice: Long,
+    val currentPrice: Long
+){
+
+    val isSamePrice: Boolean
+        get() = factoryPrice == currentPrice
+
+}
--- src/main/java/com/lannstark/lec18/Lec18Main.kt
+++ src/main/java/com/lannstark/lec18/Lec18Main.kt
@@ -3,13 +3,13 @@ package com.lannstark.lec18
 fun main() {
 
     val fruits = listOf(
-        Fruit("사과", 1_000),
-        Fruit("사과", 1_200),
-        Fruit("사과", 1_500),
-        Fruit("바나나", 3_000),
-        Fruit("바나나", 3_200),
-        Fruit("바나나", 2_500),
-        Fruit("수박", 10_000),
+        Fruit(1L, "사과", 1_000, 1_500),
+        Fruit(2L, "사과", 1_200, 1_500),
+        Fruit(3L, "사과", 1_500, 1_500),
+        Fruit(4L, "바나나", 3_000, 3_200),
+        Fruit(5L, "바나나", 3_200, 3_200),
+        Fruit(6L, "바나나", 2_500, 3_200),
+        Fruit(7L, "수박", 10_000, 10_000),
     )
 
     // filter
@@ -23,18 +23,18 @@ fun main() {
     
     // map
     fruits.filter { fruit -> fruit.name == "사과" }
-        .map { fruit -> fruit.price }
+        .map { fruit -> fruit.factoryPrice }
 
     // mapIndexed
     fruits.filter { fruit -> fruit.name == "사과" }
         .mapIndexed { idx, fruit ->
             println(idx)
-            fruit.price
+            fruit.factoryPrice
         }
 
     // mapNotNull
     fruits.filter { fruit -> fruit.name == "사과" }
-        .mapNotNull { fruit -> fruit.price }
+        .mapNotNull { fruit -> fruit.factoryPrice }
 
     // all
     fruits.all { fruit -> fruit.name == "사과" }
@@ -43,14 +43,14 @@ fun main() {
     fruits.none {fruit -> fruit.name == "사과" }
 
     // any
-    fruits.any { fruit -> fruit.price >= 10_000 }
+    fruits.any { fruit -> fruit.factoryPrice >= 10_000 }
 
     // count
     fruits.count()
 
     // sortedBy, sortedByDescending
-    fruits.sortedBy { fruit -> fruit.price }
-    fruits.sortedByDescending { fruit -> fruit.price }
+    fruits.sortedBy { fruit -> fruit.factoryPrice }
+    fruits.sortedByDescending { fruit -> fruit.factoryPrice }
 
     // distinctBy
     fruits.distinctBy { fruit -> fruit.name }
@@ -63,12 +63,53 @@ fun main() {
     fruits.last()
     fruits.lastOrNull()
 
-    // groupBy
-    val groupByMap: Map<String, List<Fruit>> = fruits.groupBy { fruit -> fruit.name }
+    // groupBy, associateBy
+    // Map<String, List<Fruit>>
+    fruits.groupBy { fruit -> fruit.name }
 
-    // associateBy
-    // val associateByMap: Map<Long, Fruit> = fruits.associateBy { fruit -> fruit.id }
+    // Map<Long, Fruit>
+    fruits.associateBy { fruit -> fruit.id }
+
+    // Map<<String, List<Long>>
+    fruits.groupBy({ fruit -> fruit.name }, { fruit -> fruit.factoryPrice })
+
+    // Map<Long, Long>
+    fruits.associateBy ({ fruit -> fruit.id }, { fruit -> fruit.factoryPrice })
+
+    // Map<String, List<Fruit>>
+    fruits.groupBy {fruit -> fruit.name}
+        .filter { (key, value) -> key == "사과" }
+
+    val fruitsInList: List<List<Fruit>> = listOf(
+        listOf(
+            Fruit(1L, "사과", 1_000, 1_500),
+            Fruit(2L, "사과", 1_200, 1_500),
+            Fruit(3L, "사과", 1_500, 1_500),
+        ),
+        listOf(
+            Fruit(4L, "바나나", 3_000, 3_200),
+            Fruit(5L, "바나나", 3_200, 3_200),
+            Fruit(6L, "바나나", 2_500, 3_200),
+        ),
+        listOf(
+            Fruit(7L, "수박", 10_000, 10_000),
+        )
+    )
+
+    // flatMap
+    fruitsInList.flatMap { list ->
+        list.filter {fruit -> fruit.factoryPrice == fruit.currentPrice }
+    }
+
+    // 확장함수를 이용한 리팩토링
+    fruitsInList.flatMap { list -> list.samePriceFilter }
+
+    // List<List<Fruit>> -> List<Fruit>
+    fruitsInList.flatten()
 }
+
+val List<Fruit>.samePriceFilter: List<Fruit>
+    get() = this.filter(Fruit::isSamePrice)
 
 private fun filterFruits(
     fruits: List<Fruit>, filter: (Fruit) -> Boolean
